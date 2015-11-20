eddb1d2b85a2487e0cbf990c09fb71174fb23500
pete warrington <pete.warrington@gmail.com>
2015-11-20 12:08:13 +0000

    SVG Examples

--- src/main/webapp/js/core/core.js
+++ src/main/webapp/js/core/core.js
@@ -13,20 +13,62 @@ function helloWorld() {
     alert("hello world!");
 }
 
+
 function buildChart() {
-    console.log("building a chart");   
-    var data = [4, 8, 15, 16, 23, 42];
+    console.log("building a chart");
+    var width = 420,
+            height = 500;
+
     var x = d3.scale.linear()
-            .domain([0, d3.max(data)])
-            .range([0, 420]);
-    d3.select(".chart")
-            .selectAll("div")
-            .data(data)
-            .enter().append("div")
-            .style("width", function (d) {
-                return x(d) + "px";
-            })
-            .text(function (d) {
-                return d;
-            });
+            .range([0, width]);
+
+    var width = 960,
+            height = 500;
+
+    var y = d3.scale.linear()
+            .range([height, 0]);
+
+    var chart = d3.select(".chart")
+            .attr("width", width)
+            .attr("height", height);
+
+    d3.tsv("d3.tsv", type, function (error, data) {
+        y.domain([0, d3.max(data, function (d) {
+                return d.value;
+            })]);
+
+        var barWidth = width / data.length;
+
+        var bar = chart.selectAll("g")
+                .data(data)
+                .enter().append("g")
+                .attr("transform", function (d, i) {
+                    return "translate(" + i * barWidth + ",0)";
+                });
+
+        bar.append("rect")
+                .attr("y", function (d) {
+                    return y(d.value);
+                })
+                .attr("height", function (d) {
+                    return height - y(d.value);
+                })
+                .attr("width", barWidth - 1);
+
+        bar.append("text")
+                .attr("x", barWidth / 2)
+                .attr("y", function (d) {
+                    return y(d.value) + 3;
+                })
+                .attr("dy", ".75em")
+                .text(function (d) {
+                    return d.value;
+                });
+    });
+
+    function type(d) {
+        d.value = +d.value; // coerce to number
+        return d;
+    }
+
 }
